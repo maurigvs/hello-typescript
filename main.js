60a550cad1aea3f454ac148fa6bb7a81395c2eb6
Mauri Gonçalves <maurigvs@gmail.com>
2022-02-09 21:47:49 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var minhaVar = "minha variavel";
+function minhaFunc(x, y) {
+    return x + y;
+}
+// ES 6 ou ES 2015
+var num = 2;
+var PI = 3.14;
+var numero = [1, 2, 3];
+numeros.map(function (valor) { });
+{
+    return valor * 2;
+}
+numeros.map(function (valor) { return valor * 2; });
---
+++ main.ts
@@ -0,0 +1,17 @@
+var minhaVar = "minha variavel";
+
+function minhaFunc(x, y){
+    return x + y;
+}
+
+// ES 6 ou ES 2015
+let num = 2;
+const PI = 3.14;
+
+var numero = [1, 2, 3];
+
+numeros.map(function(valor)){
+    return valor * 2;
+}
+
+numeros.map(valor => valor *2); 
